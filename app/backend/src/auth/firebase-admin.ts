66ad90467f282935f6e251b0296492690de204ae
Felipe Silberberg <felipe.silberberg@sou.inteli.edu.br>
2025-11-17 14:34:55 -0300

    fix: firebase connection

--- app/backend/src/auth/firebase-admin.ts
+++ app/backend/src/auth/firebase-admin.ts
@@ -1,6 +1,12 @@
 import * as admin from 'firebase-admin';
-import * as serviceAccount from './secrets/firebase-service-account.json';
+
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+};
+
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
 export { admin };
